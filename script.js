e5991b70ba7a4642ea54dd0b565e729abd452f41
Patrick Rolon <patrolon1@yahoo.com>
2018-01-11 10:09:16 -0500

    added working buttons and music file

---
+++ script.js
@@ -0,0 +1,17 @@
+var buttons = 
+    {
+        playButton: function()
+        {
+        play.play();
+        }
+        ,
+        
+        pause: function()
+        {
+        alert("test 2d")
+        }
+    }
+
+var play = document.getElementById("rock");
+
+play.play();
